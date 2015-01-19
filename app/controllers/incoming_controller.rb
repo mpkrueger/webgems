f04ce185799a6dd31d581f1f5eb3c28340cd4b37
mpkrueger <mpkrueger@gmail.com>
2015-01-19 15:18:23 -0800

    fix

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,7 +2,6 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-    puts "INCOMING PARAMS HERE: #{params}"
     @user = User.find_by(email: params[:sender])
     @topic = Topic.find_by(title: params[:subject])
     @url = params["body-plain"]
