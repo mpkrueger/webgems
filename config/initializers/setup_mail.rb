1c57b2d8d80d2f8877143c2d5745ac2fea4d3a83
mpkrueger <mpkrueger@gmail.com>
2015-01-19 14:13:49 -0800

    switched to mailgun

--- config/initializers/setup_mail.rb
+++ config/initializers/setup_mail.rb
@@ -1,12 +1,24 @@
+ActionMailer::Base.smtp_settings = {
+  port: 587,
+  address: 'smtp.mailgun.org',
+  user_name: ENV['MAILGUN_SMTP_LOGIN'],
+  password: ENV['MAILGUN_SMTP_PASSWORD'],
+  domain: 'intense-tor-4200.herokuapp.com',
+  authentication: :plain,
+  content_type: 'text/html'
+}
+ActionMailer::Base.delivery_method = :smtp
+
+ActionMailer::Base.raise_delivery_errors = true
+
+class DevelopmentMailInterceptor
+  def self.delivering_email(message)
+    message.to = 'mpkrueger@gmail.com'
+    message.cc = nil
+    message.bcc = nil
+  end
+end
+
 if Rails.env.development?
-  ActionMailer::Base.delivery_method = :smtp
-  ActionMailer::Base.smtp_settings = {
-    address: 'smtp.sendgrid.net',
-    port: '587',
-    authentication: :plain,
-    user_name: ENV['SENDGRID_USERNAME'],
-    password: ENV['SENDGRID_PASSWORD'],
-    domain: 'heroku.com',
-    enable_starttls_auto: true
-  }
+  ActionMailer::Base.register_interceptor(DevelopmentMailInterceptor)
 end
