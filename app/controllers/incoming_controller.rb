a2ca89c85ac85eb95406f82eebbe31b6caf78841
mpkrueger <mpkrueger@gmail.com>
2015-01-19 14:50:09 -0800

    changed find_by method in incoming#create

--- app/controllers/incoming_controller.rb
+++ app/controllers/incoming_controller.rb
@@ -2,9 +2,9 @@ class IncomingController < ApplicationController
   skip_before_action :verify_authenticity_token, only: [:create]
 
   def create
-
-    @user = User.find(params[:sender])
-    @topic = Topic.find(params[:subject])
+    puts "INCOMING PARAMS HERE: #{params}"
+    @user = User.find_by(email: params[:sender])
+    @topic = Topic.find_by(title: params[:subject])
     @url = params["body-plain"]
 
     if user.nil?
